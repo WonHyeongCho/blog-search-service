916ebb63e71ce64aefc80c51ada53dd0137a7de2
조원형/보안플랫폼팀(YA) <wonhyeong.cho@yanolja.com>
2023-11-15 16:29:41 +0900

    DB에 키워드 카운트 저장하는 로직 이벤트로 변경

--- src/main/kotlin/my/blogsearchservice/service/SearchKeywordStatsService.kt
+++ src/main/kotlin/my/blogsearchservice/service/SearchKeywordStatsService.kt
@@ -1,6 +1,5 @@
 package my.blogsearchservice.service
 
-import jakarta.transaction.Transactional
 import my.blogsearchservice.entity.SearchKeywordStat
 import my.blogsearchservice.repository.SearchKeywordStatsRepository
 import org.springframework.stereotype.Service
@@ -12,20 +11,5 @@ class SearchKeywordStatsService(
 
     fun getTop10SearchKeywordStat(): List<SearchKeywordStat> {
         return searchKeywordStatsRepository.findTop10ByOrderByCountDesc()
-    }
-
-    @Transactional
-    fun addSearchKeywordStat(keyword: String) {
-        val searchKeywordStat = getSearchKeywordStatWithLock(keyword) ?: SearchKeywordStat(keyword)
-        searchKeywordStat.incrementCount()
-        saveSearchKeywordStat(searchKeywordStat)
-    }
-
-    private fun saveSearchKeywordStat(searchKeywordStat: SearchKeywordStat) {
-        searchKeywordStatsRepository.save(searchKeywordStat)
-    }
-
-    private fun getSearchKeywordStatWithLock(keyword: String): SearchKeywordStat? {
-        return searchKeywordStatsRepository.findByIdWithLock(keyword)
     }
 }
